d8ee41035d7952ac4cc991fee676b85d005e1565
chisssel <platov.artem.15@gmail.com>
2025-09-18 23:29:44 +0300

    Ready-made logic and example

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"PaymentModuleLogic/payments"
+	"PaymentModuleLogic/payments/methods"
+	"github.com/k0kubun/pp"
+)
 
+func main() {
+	method := methods.NewCrypto()
+
+	paymentModule := payments.NewPaymentModule(method)
+
+	paymentModule.Pay("Бургер", 5)
+	idPhone := paymentModule.Pay("Телефон", 500)
+	idGame := paymentModule.Pay("Игра", 20)
+
+	paymentModule.Cancel(idPhone)
+
+	allInfo := paymentModule.AllInfo()
+	pp.Println(allInfo)
+
+	gameInfo := paymentModule.Info(idGame)
+	pp.Println("Game info:", gameInfo)
 }
